1a5aea4dc7f4d117766188cc3c29912353fc6461
Emanuel Turis <hello@emanuelturis.com>
2025-06-19 22:16:31 +0300

    add stuff

--- lua/mason-lspconfig/mappings.lua
+++ lua/mason-lspconfig/mappings.lua
@@ -8,11 +8,12 @@ function M.get_mason_map()
     local package_to_lspconfig = {}
     for _, pkg_spec in ipairs(registry.get_all_package_specs()) do
         local lspconfig = vim.tbl_get(pkg_spec, "neovim", "lspconfig")
-        print(vim.inspect(pkg_spec.name))
         if lspconfig then
             package_to_lspconfig[pkg_spec.name] = lspconfig
         end
     end
+
+    print(vim.inspect(package_to_lspconfig))
 
     return {
         package_to_lspconfig = package_to_lspconfig,
